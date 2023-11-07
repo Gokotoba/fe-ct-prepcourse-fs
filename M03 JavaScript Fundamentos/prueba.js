6bca38268b17bbaabaee61735fa457c4f808c4fd
GonzaPugna <gonza.pugnaloni@gmail.com>
2023-11-07 12:35:24 -0300

    Homework 03

---
+++ M03 JavaScript Fundamentos/prueba.js
@@ -0,0 +1,11 @@
+function tienenMismaLongitud(str1, str2) {
+    if ("str1" === "str2" ){
+       return true;
+    }
+    else {
+       return false;
+    }
+ }
+var str1 = pariente;
+var str2 = tomas;
+console.log ();
